275d5007b8cd9d47810216adeafe108c8bafa2a4
sayaya <sayaya1090@hotmail.com>
2023-08-28 07:58:13 +0900

    .

--- src/main/java/net/sayaya/rx/Observer.java
+++ src/main/java/net/sayaya/rx/Observer.java
@@ -4,13 +4,13 @@ import elemental2.dom.DomGlobal;
 import jsinterop.annotations.*;
 
 @JsType(isNative = true, namespace="rxjs", name="Observer")
-@JsFunction
 public interface Observer<T> {
     @JsMethod void next(T value);
     @JsMethod void error(Throwable error);
     @JsMethod void complete();
 
     @JsFunction
+    @FunctionalInterface
     interface ObserverDefault<T> extends Observer<T> {
         default void error(Throwable error) {}
         default void complete() {}
--- src/main/java/net/sayaya/rx/Subscription.java
+++ src/main/java/net/sayaya/rx/Subscription.java
@@ -1,10 +1,9 @@
 package net.sayaya.rx;
 
-import jsinterop.annotations.JsFunction;
 import jsinterop.annotations.JsType;
 
 @JsType(isNative = true, namespace="rxjs", name="Subscription")
-@JsFunction
+//@JsFunction
 public class Subscription {
     native void unsubscribe();
 }
