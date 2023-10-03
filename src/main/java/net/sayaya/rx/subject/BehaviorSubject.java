f7c6dd3f3bd63f64779abf51038685c114e04040
sayaya <sayaya1090@hotmail.com>
2023-10-03 23:30:26 +0900

    .

--- src/main/java/net/sayaya/rx/subject/BehaviorSubject.java
+++ src/main/java/net/sayaya/rx/subject/BehaviorSubject.java
@@ -1,11 +1,16 @@
 package net.sayaya.rx.subject;
 
 import lombok.experimental.Delegate;
+import net.sayaya.rx.HasValueChangeHandlers;
 
-public class BehaviorSubject<T> {
+public class BehaviorSubject<T> implements HasValueChangeHandlers.ValueChangeEventListener<T> {
     public static <T> BehaviorSubject<T> behavior(T init) {
         return new BehaviorSubject<>(init);
     }
     @Delegate private final BehaviorSubjectJs<T> delegate;
     public BehaviorSubject(T init) { delegate = new BehaviorSubjectJs<>(init); }
+    @Override
+    public void handle(HasValueChangeHandlers.ValueChangeEvent<T> evt) {
+        delegate.next(evt.value());
+    }
 }
--- src/main/java/net/sayaya/rx/subject/Subject.java
+++ src/main/java/net/sayaya/rx/subject/Subject.java
@@ -1,17 +1,16 @@
 package net.sayaya.rx.subject;
 
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import lombok.experimental.Delegate;
+import net.sayaya.rx.HasValueChangeHandlers;
 
-public class Subject<T> implements ValueChangeHandler<T> {
+public class Subject<T> implements HasValueChangeHandlers.ValueChangeEventListener<T> {
     public static <T> Subject<T> subject(Class<T> clazz) {
         return new Subject<>();
     }
     @Delegate private final SubjectJs<T> delegate;
     public Subject() { delegate = new SubjectJs<>(); }
     @Override
-    public void onValueChange(ValueChangeEvent<T> event) {
-        delegate.next(event.getValue());
+    public void handle(HasValueChangeHandlers.ValueChangeEvent<T> evt) {
+        delegate.next(evt.value());
     }
 }
