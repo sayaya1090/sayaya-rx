279d036937d9bb88754ffa462701121ac6f09d13
sayaya <sayaya1090@hotmail.com>
2024-11-12 03:53:11 +0900

    임시

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -1,55 +1,40 @@
+import org.docstr.gwt.GwtBaseTask
+
 plugins {
     id("java")
-    id("org.docstr.gwt") version "1.1.30"
-    id("war")
-    id("gwt")
+    id("org.docstr.gwt") version "2.0.12"
 }
 repositories {
     mavenCentral()
     mavenLocal()
 }
 group = "net.sayaya"
-version = "1.7"
-java.sourceCompatibility = JavaVersion.VERSION_21
-java.targetCompatibility = JavaVersion.VERSION_21
-sourceSets {
-    getByName("main").java.srcDirs(
-        "build/generated/sources/annotationProcessor/java/main"
-    )
-}
+version = "2.0"
+
 dependencies {
-    compileOnly(project(":"))
+    implementation(project(":"))
     annotationProcessor(project(":"))
-    implementation("org.jboss.elemento:elemento-core:1.6.1")
-    implementation("org.gwtproject:gwt-user:2.11.0")
-    compileOnly("org.gwtproject:gwt-dev:2.11.0")
-    implementation("org.projectlombok:lombok:1.18.32")
-    annotationProcessor("org.projectlombok:lombok:1.18.32")
+    implementation("org.jboss.elemento:elemento-core:1.6.10")
+    implementation("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
 }
+
 tasks {
     withType<Delete> { doFirst { delete("build/") } }
     withType<JavaCompile> {
         options.encoding = "UTF-8"
     }
     gwt {
-        minHeapSize = "1024M"
-        maxHeapSize = "2048M"
-        sourceLevel = "auto"
+        gwtVersion = "2.12.0"
         gwt.modules = listOf("net.sayaya.Test")
-    }
-    val lombok: File = project.configurations.annotationProcessor.get().filter { it.name.startsWith("lombok") }.single()
-    compileGwt {
-        extraJvmArgs = listOf("-XX:ReservedCodeCacheSize=512M", "-javaagent:${lombok}=ECJ")
-    }
-    gwtDev {
-        minHeapSize = "4096M"
-        maxHeapSize = "4096M"
         sourceLevel = "auto"
-        extraJvmArgs = listOf("-XX:ReservedCodeCacheSize=512M", "-javaagent:${lombok}=ECJ")
-        port = 8888
-        war = File("src/main/webapp")
+        devMode {
+            port = 8888
+            war = File("src/main/webapp")
+        }
     }
-    withType<War> {
-        duplicatesStrategy = DuplicatesStrategy.WARN
+    withType<GwtBaseTask> {
+        val lombok: File = project.configurations.annotationProcessor.get().filter { it.name.startsWith("lombok") }.single()
+        jvmArgs = listOf("-javaagent:${lombok}=ECJ")
     }
 }
